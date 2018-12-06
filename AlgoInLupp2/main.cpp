ada493c225cbc3e402c034a2add79dee72cdf867
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-06 11:26:10 +0100

    Update main.cpp

--- AlgoInLupp2/main.cpp
+++ AlgoInLupp2/main.cpp
@@ -5,12 +5,49 @@
 #include "Stack.h"
 #include "DoubleLinkedList.h"
 #include "LinkedList.h"
-
+#include <iostream>
 using namespace std; 
 
 
-int main()
+
+int main(int argc, char **argv)
 {
+    Stack<int> *stack = new Stack<int>();
+    Stack<float> stackFloat;
+
+    for(int i = 0; i < 10; i++)
+    {
+        stack->push(i);
+    }
+    
+    cout << "size of Stack integer = " << stack->size() << endl;
+    cout << "Stack Float = " << *stack << endl;
+    
+    if(stackFloat.empty())
+        cout << "Stack Float is Empty" << endl;
+    else 
+        cout << "Stack Float is not Empty" << endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        stackFloat.push(0.5f);
+    }
+    
+    cout << "Stack Float = " << stackFloat << endl;    
+    
+    stackFloat.pop();
+    cout << "Stack Float = " << stackFloat << endl;  
+    cout << "Stack Float size = " << stackFloat.size() << endl;
+
+    stack->invert();
+    cout << "Stack integer = " << *stack << endl;  
+    cout << "Stack integer size = " << stack->size() << endl;
+
+    delete stack;
+    return 0;
+}
+
+
 	while (true)
 	{
 		cout << endl;  system("pause");
