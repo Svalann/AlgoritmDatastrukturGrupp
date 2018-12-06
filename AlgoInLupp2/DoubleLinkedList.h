9e491f4149a65ef17ce841c27dcda16e547d8743
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-06 11:28:40 +0100

    Update DoubleLinkedList.h

--- AlgoInLupp2/DoubleLinkedList.h
+++ AlgoInLupp2/DoubleLinkedList.h
@@ -1,8 +1,37 @@
 #pragma once
+#ifndef _DOUBLE_LINKED_LIST_H_
+#define _DOUBLE_LINKED_LIST_H_
+#include <iostream>
+#include "Binode.h" // Create new Binode header file
+
+template<class T>
 class DoubleLinkedList
 {
-public:
-	DoubleLinkedList();
-	~DoubleLinkedList();
-};
+    private:
+        Binode<T> *_first, *_last;
+        int _size;
 
+    public:
+        DoubleLinkedList() : _first(NULL), _last(NULL), _size(0) {};
+        DoubleLinkedList(const DoubleLinkedList&);
+        ~DoubleLinkedList() { clear(); };
+
+        bool empty() const { return _size == 0; };
+        int size() const { return _size; };
+        bool sorted() const;
+        T front() const;
+        T back() const;
+
+        void pushFront(T);
+        void pushBack(T);
+        void popFront();
+        void popBack();
+        void erase(int);
+        void change(T, int);
+        void clear();
+        void invert();
+        void sort();
+        void bubblesort();
+        void quicksort();
+	
+	///continue
