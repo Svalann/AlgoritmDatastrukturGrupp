fd0e2252af31a54d8dfc162fcdaeed7b57ade686
NaimaClaesson91 <32983353+NaimaClaesson91@users.noreply.github.com>
2018-12-12 14:08:17 +0100

    added more tests to my LL

--- AlgoInlupp2-Tests/Test-LinkedList.cpp
+++ AlgoInlupp2-Tests/Test-LinkedList.cpp
@@ -6,8 +6,52 @@ TEST(TestLinked, testingGetSize) {
 
     LinkedList<int> theList;
     theList.AddFirst(1);
+
     EXPECT_EQ(1,theList.GetSize());
 }
+
+TEST(TestLinked, testingRemove) {
+
+	LinkedList<int> theList;
+	theList.AddFirst(1);
+	theList.AddFirst(2);
+	theList.RemovePosition(0);
+
+	EXPECT_EQ(1, theList.GetSize());
+}
+
+TEST(TestLinked, testingGetNode) {
+
+	LinkedList<int> theList;
+	theList.AddFirst(1);
+	theList.AddFirst(2);
+	theList.AddFirst(3);
+
+
+	EXPECT_EQ(3, theList.GetNode(0));
+}
+
+TEST(TestLinked, testingAddLast) {
+
+	LinkedList<int> theList;
+	theList.AddFirst(1);
+	theList.AddFirst(2);
+	theList.AddFirst(3);
+	theList.AddLast(10);
+
+
+	EXPECT_EQ(10, theList.GetNode(3));
+}
+
+//TEST(TestLinked, testingInsert) {
+//
+//	LinkedList<int> theList;
+//	theList.AddFirst(1);
+//	theList.AddLast(3);
+//	theList.InsertPosition(2, 1);
+//
+//	EXPECT_EQ(2, theList.GetNode(1));
+//}
 
 
 
