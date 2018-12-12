587c26f801f3eb17659d938eb189275b8a9d9ebe
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-12 19:22:33 +0100

    Update DoubleLinkedList.h

--- AlgoInLupp2/DoubleLinkedList.h
+++ AlgoInLupp2/DoubleLinkedList.h
@@ -1,19 +1,19 @@
-#ifndef _DOUBLY_LINKED_LIST_H_
-#define _DOUBLY_LINKED_LIST_H_
+#ifndef _DOUBLE_LINKED_LIST_H_
+#define _DOUBLE_LINKED_LIST_H_
 #include <iostream>
 #include "Binode.h"
 
 template<class T>
-class DoublyLinkedList
+class DoubleLinkedList
 {
     private:
         Binode<T> *_first, *_last;
         int _size;
 
     public:
-        DoublyLinkedList() : _first(NULL), _last(NULL), _size(0) {};
-        DoublyLinkedList(const DoublyLinkedList&);
-        ~DoublyLinkedList() { Clear(); };
+        DoubleLinkedList() : _first(NULL), _last(NULL), _size(0) {};
+        DoubleLinkedList(const DoubleLinkedList&);
+        ~DoubleLinkedList() { Clear(); };
         void Clear();
         bool Empty() const { return _size == 0; };
         int GetSize() const { return _size; };
