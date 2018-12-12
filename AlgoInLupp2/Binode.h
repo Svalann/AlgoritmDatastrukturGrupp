abfffe0c2802d404f07d1a6d6696ae2fad416493
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-12 19:23:27 +0100

    Update Binode.h

--- AlgoInLupp2/Binode.h
+++ AlgoInLupp2/Binode.h
@@ -5,15 +5,15 @@
 template<class T>
 class Binode
 {
-  private:
+    private:
         T _key;
         Binode *_prev, *_next;
-  
-  public:
+
+    public:
         Binode() : _prev(NULL), _next(NULL) {};
         Binode(const Binode& _in) : _key(_in._key), _prev(NULL), _next(NULL) {};
         Binode(T e) : _key(e), _prev(NULL), _next(NULL) {};
-        
+
         void setKey(T e) { _key = e; };
         void setPrev(Binode *_in) { _prev = _in; };
         void setNext(Binode *_in) { _next = _in; };
@@ -23,9 +23,3 @@ class Binode
         Binode *getNext() const { return _next; };
 
         static void swap(Binode *, Binode *);
-};
-
-template<class T>
-void Binode<T>::swap(Binode *a, Binode *b)
-
-#endif
