a306883e8e67847d16e79e7617104cc8e9ca232d
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-12 19:17:24 +0100

    Update Node.h

--- AlgoInLupp2/Node.h
+++ AlgoInLupp2/Node.h
@@ -18,72 +18,17 @@ class Node
         Node(T _in) : _data(_in), _next(NULL) { };
         //Destroy
         ~Node() { };
-        
+        //Methods Setters
         void setKey(T _in) { _data = _in; };
         void setNext(Node *n) { _next = n; };
-       
+        //Methods Getters
         T getKey() const { return _data; };
         Node* getNext() const { return _next; };
 
-        
-        bool operator>(const Node &_in);
-        bool operator<(const Node &_in);
-        bool operator==(const Node &_in);
-        bool operator>=(const Node &_in);
-        bool operator<=(const Node &_in);
-
-       
+        //swap method
         static void swap(Node *_inA, Node *_inB);
 };
 
 
-template<class T>
-bool Node<T>::operator>(const Node<T> &a)
-{
-    if (this != &a)
-        return (this->key() > a.key());
-
-    return false;
-}
-
-
-template<class T>
-bool Node<T>::operator<(const Node<T> &a )
-{
-    if (this != &a)
-        return (this->key() < a.key());
-
-    return false;
-}
-
-
-template<class T>
-bool Node<T>::operator==(const Node<T> &a)
-{
-    return (this->key() == a.key());
-}
-
-
-template<class T>
-bool Node<T>::operator>=(const Node<T> &a)
-{
-    return (*this > a || *this == a);
-}
-
-
-template<class T>
-bool Node<T>::operator<=(const Node<T> &a)
-{
-    return (*this < a || *this == a);
-}
-
-
-template<class T>
-void Node<T>::swap(Node<T> *a, Node<T> *b)
-{
-    T aux = a->_data;
-    a->_data = b->_data;
-    b->_data = aux;
-}
-
 #endif
+
