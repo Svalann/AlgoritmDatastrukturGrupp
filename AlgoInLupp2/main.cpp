e0f1f7c694e82ad37664a7aae35ca3efc3924c5c
Melikinho <45233645+Melikinho@users.noreply.github.com>
2018-12-13 10:09:10 +0100

    Update main.cpp

--- AlgoInLupp2/main.cpp
+++ AlgoInLupp2/main.cpp
@@ -5,40 +5,63 @@
 #include "DoubleLinkedList.h"
 #include "LinkedList.h"
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-
-
-
-void testStack(Stack<int> stack)
+void Test(DoubleLinkedList<int> doublelinkedlist)
 {
-    cout << "Stack integer size = " << stack.size() << endl; 
+    Stack<int> stack = Stack<int>();
 
     if(stack.empty())
         cout << "Stack integer is Empty" << endl;
     else 
         cout << "Stack integer is not Empty" << endl;
 
+    if(doublelinkedlist.Empty())
+        cout << "doublelinkedlist integer is Empty" << endl;
+    else 
+        cout << "doublelinkedlist integer is not Empty" << endl;
+        
+    cout << "\nthe stack is filled with 10 integers" << endl;
     for(int i = 0; i < 10; i++)
     {
         stack.push(i);
-        cout << stack.top() << endl;
     }
 
-    if(stack.empty())
-        cout << "Stack integer is Empty" << endl;
+    cout << "\nStack is Copied in DoubleLinkedList" << endl;
+    cout << "Stack integer size = " << stack.size() << endl;    
+    cout << "DoubleLinkedList integer size = " << doublelinkedlist.GetSize() << endl;
+    cout << "DoubleLinkedList integer = ";
+    for (int i = 1; i <= 10; i++)
+    {
+        doublelinkedlist.InsertAtPosition(i, stack.top());
+        cout << doublelinkedlist.GetAtPosition(i) << "->";
+        stack.pop();
+    }
+
+    doublelinkedlist.RemoveAtPosition(5);
+
+    cout << "\nDoubleLinkedList remove position 5" << endl;
+    cout << "DoubleLinkedList integer = ";
+    for (int i = 1; i <= doublelinkedlist.GetSize(); i++)
+    {
+        cout << doublelinkedlist.GetAtPosition(i) << "->";
+    }
+
+    if(doublelinkedlist.Empty())
+        cout << "\nDoubleLinkedList integer is Empty" << endl;
     else 
-        cout << "Stack integer is not Empty" << endl;
-
-    cout << "Stack integer size = " << stack.size() << endl;
+        cout << "\nDoubleLinkedList integer is not Empty" << endl;
+    
+    cout << "DoubleLinkedList integer size = " << doublelinkedlist.GetSize() << endl;
+    cout << "A END process" << endl;
 }
-
 
 int main(int argc, char **argv)
 {
-    Stack<int> stack = Stack<int>();    
+    DoubleLinkedList<int> list;
 
-    testStack(stack);    
+    Test(list);
+
     return 0;
 }
 
